99e5ee182e3d5c4a000cba0f18f01300382516fa
Михаил Кормановский <kormanowsky@gmail.com>
2022-11-30 03:40:19 +0300

    fix: must be less or equal 1000000

--- src/vkApi/kickMember.ts
+++ src/vkApi/kickMember.ts
@@ -5,10 +5,10 @@ export type KickUserParams = {
     member_id: number,
     peer_id: number
 }
-export default function kickMember({member_id, peer_id: chat_id}: KickUserParams) {
+export default function kickMember({member_id, peer_id}: KickUserParams) {
     return callVKAPI<MessagesRemoveChatUserParams, MessagesRemoveChatUserResponse>(
         "messages.removeChatUser", {
             member_id,
-            chat_id
+            chat_id: peer_id - 2000000000 // must be less or equal 100000000
         });
 }
