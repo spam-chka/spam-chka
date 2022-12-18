ca28ccb0c3472e75905ff53d5a93adf6313f8d37
Михаил Кормановский <kormanowsky@gmail.com>
2022-12-18 20:25:01 +0300

    add: new strings

--- src/l18n.ts
+++ src/l18n.ts
@@ -14,11 +14,11 @@ export type SpamChkaLocalizations = {
 
 const localizations: SpamChkaLocalizations = {
     en: {
-        confirmMessage: "Hey, @{member_id}! Sorry, you look like a bot. Confirm you're not or I'll have to kick you :(\nMinute(s) left: {kick_delay}.",
+        confirmMessage: "Hey, @{member_id}! Confirm you're not a ot or I'll have to kick you :(\nMinute(s) left: {kick_delay}.",
         confirmButton: "I am not a bot"
     },
     ru: {
-        confirmMessage: "Добро пожаловать, @{member_id}! К сожалению, Ваш аккаунт выглядит как бот. Подтвердите, что это не так или мне придется исключить Вас :(\nОсталось минут: {kick_delay}.",
+        confirmMessage: "Добро пожаловать, @{member_id}! Подтвердите, что Вы не бот или мне придется исключить Вас :(\nОсталось минут: {kick_delay}.",
         confirmButton: "Я не бот"
     }
 }
