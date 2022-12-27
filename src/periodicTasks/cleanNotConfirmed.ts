c7d9424a0a889f20a206076f30ae62ff3f778857
Михаил Кормановский <kormanowsky@gmail.com>
2022-12-28 02:25:27 +0300

    fix: deleting all users (including ones who has confirmed)

--- src/periodicTasks/cleanNotConfirmed.ts
+++ src/periodicTasks/cleanNotConfirmed.ts
@@ -4,13 +4,33 @@ import {getTimestamp} from "../timestamps";
 import {KICK_UNCONFIRMED_THRESHOLD_SECONDS} from "../config";
 
 export default function cleanNotConfirmed() {
-    Event.find({
-        type: Event.EVENT_AWAIT_CONFIRM, ts: {
-            $gte: getTimestamp() - KICK_UNCONFIRMED_THRESHOLD_SECONDS
+    Event.aggregate([
+        {
+            $match: {
+                ts: {
+                    $gte: getTimestamp() - KICK_UNCONFIRMED_THRESHOLD_SECONDS
+                }
+            }
+        },
+        {
+            $sort: {
+                ts: 1
+            }
+        },
+        {
+            $group: {
+                _id: {peer_id: "$peer_id", member_id: "$member_id"},
+                last_event: {
+                    $last: "$$CURRENT"
+                }
+            }
         }
-    }).then(events => {
-        events.forEach(event => {
-            kickMemberAndDeleteMessage(event as Event, event.meta.confirm_id);
+    ])
+        .then(groups => {
+            groups.forEach(({last_event}) => {
+                if(last_event.type === Event.EVENT_AWAIT_CONFIRM) {
+                    kickMemberAndDeleteMessage(last_event, last_event.meta.confirm_id);
+                }
+            });
         });
-    });
 }
